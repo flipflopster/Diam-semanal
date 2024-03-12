2c54e0fa9a544389105274bb132a511dd6f722ac
Camarão <ghalopes19@gmail.com>
2024-03-12 10:06:37 +0000

    silly

--- Semana 5/Gui/sitegoat/votacao/views.py
+++ Semana 5/Gui/sitegoat/votacao/views.py
@@ -1,8 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Questao
+from .models import Questao, Opcao
 
+def voto(request, questao_id):
+    questao = get_object_or_404(Questao, pk=questao_id)
+    try:
+        opcao_seleccionada = questao.opcao_set.get(pk=request.POST['opcao'])
+    except (KeyError, Opcao.DoesNotExist):
+        # Apresenta de novo o form para votar
+        return render(request, 'votacao/detalhe.html', { 'questao': questao, 'error_message': "Não escolheu uma opção"})
+    else:
+        opcao_seleccionada.votos += 1
+        opcao_seleccionada.save()
+        # Retorne sempre HttpResponseRedirect depois de
+        # tratar os dados POST de um form
+        # pois isso impede os dados de serem tratados
+        # repetidamente se o utilizador
+        # voltar para a página web anterior.
+    return HttpResponseRedirect(reverse('votacao:resultados', args=(questao.id,)))
 
 def index(request):
     latest_question_list = Questao.objects.order_by('-pub_data')[:5]
@@ -16,15 +33,5 @@ def detalhe(request, questao_id):
 
 
 def resultados(request, questao_id):
-    response = "Estes sao os resultados da questao %s."
-    return HttpResponse(response % questao_id)
-
-
-def voto(request, questao_id):
-    return HttpResponse("Votacao na questao %s." % questao_id)
-
-
-def index(request):
-    return HttpResponse("Pagina de entrada da app votacao.")
-
-# Create your views here.
+    questao = get_object_or_404(Questao, pk=questao_id)
+    return render(request, 'votacao/resultados.html', {'questao': questao})
