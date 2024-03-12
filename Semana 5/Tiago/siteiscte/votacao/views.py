5a2ee47ed39cc4e007908497873028a900221e03
tiagof2001 <159270739+tiagof2001@users.noreply.github.com>
2024-03-12 10:10:15 +0000

    update

--- Semana 5/Tiago/siteiscte/votacao/views.py
+++ Semana 5/Tiago/siteiscte/votacao/views.py
@@ -1,6 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Questao, Opcao
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Página de entrada da app votacao.")
+    latest_question_list = Questao.objects.order_by('-pub_data')[:5]
+    # template = loader.get_template('votacao/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    #  return HttpResponse(template.render(context, request))
+    return render(request, 'votacao/index.html', context)
+
+
+def detalhe(request, questao_id):
+    questao = get_object_or_404(Questao, pk=questao_id)
+    return render(request, 'votacao/detalhe.html', {'questao': questao})
+
+
+def resultados(request, questao_id):
+    questao = get_object_or_404(Questao, pk=questao_id)
+    return render(request, 'votacao/resultados.html', {'questao': questao})
+
+
+def voto(request, questao_id):
+    questao = get_object_or_404(Questao, pk=questao_id)
+    try:
+        opcao_seleccionada = questao.opcao_set.get(pk=request.POST['opcao'])
+    except (KeyError, Opcao.DoesNotExist):
+        # Apresenta de novo o form para votar
+        return render(request, 'votacao/detalhe.html', {
+            'questao': questao,
+            'error_message': "Não escolheu uma opção",
+        })
+    else:
+        opcao_seleccionada.votos += 1
+        opcao_seleccionada.save()
+        # Retorne sempre HttpResponseRedirect depois de
+        # tratar os dados POST de um form
+        # pois isso impede os dados de serem tratados
+        # repetidamente se o utilizador
+        # voltar para a página web anterior.
+    return HttpResponseRedirect(
+        reverse('votacao:resultados', args=(questao.id,)))
