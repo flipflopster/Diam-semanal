bc935a68b1411727a7416bb20077e19bbf86b5f2
Camarão <ghalopes19@gmail.com>
2024-04-22 18:37:01 +0100

    semana 8

--- Semana 8/Gui/sitegoat/votacao/static/script.js
+++ Semana 8/Gui/sitegoat/votacao/static/script.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-    $("#logged").click(function() {
-        $("#logged").hide();
+
+    $("#logged").on("dblclick", (function() {
+        $(this).fadeOut(400);
+    }));
+    $("#name").click(function() {
+        $("#logged").fadeIn(400);
     });
+
+
+    $("#lstButton").click(function() {
+        if( $("#lst").text().equals("Mostrar Lista de Quetsão")) {
+            $("#lst").text("Esconder Lista de Questrão");
+            $("#lst").show();
+        } else {
+            $("#lst").text("Mostrar Lista de Quetsão");
+            $("#lst").hide();
+        }
+    });
+    //    <script src="{% static 'script.js' %}"></script>
 });
