b379ff4d9fce2eccfa2b345b2e1104a98967b828
flipflopster <146463488+flipflopster@users.noreply.github.com>
2024-04-29 15:24:03 +0100

    Revert "updated models.py"
    This reverts commit c10ed307102765a7ef91247cd8a67291377c2cf3.

--- newbix/newbix/gameapp/models.py
+++ newbix/newbix/gameapp/models.py
@@ -27,24 +27,21 @@ class Utilizador(models.Model):
 
     profile_picture = models.CharField(max_length=100)
 
-    
-
 
 class Lista_Amigos(models.Model):
-    utilizador_id = models.ForeignKey(Utilizador, on_delete=models.CASCADE)
-    utilizador_seguido_id = models.ForeignKey(Utilizador, on_delete=models.CASCADE)
+    utilizador_id = models.ForeignKey(Utilizador, on_delete=models.CASCADE, related_name='utilizador_id')
+    utilizador_seguido_id = models.ForeignKey(Utilizador, on_delete=models.CASCADE,
+                                              related_name='utilizador_seguido_id')
 
     def nomes(self):
         def get_username_attributes(utilizador_id, utilizador_seguido_id):
             utilizador = Utilizador.objects.get(id=utilizador_id)
             utilizador_seguido = Utilizador.objects.get(id=utilizador_seguido_id)
             return utilizador.username, utilizador_seguido.username
-        
+
         username1, username2 = get_username_attributes(self.utilizador_id, self.utilizador_seguido_id)
-        
+
         return username1, username2
-
-
 
 
 class Jogo(models.Model):
@@ -55,22 +52,21 @@ class Jogo(models.Model):
 
     def __str__(self):
         return self.nome
-    
+
     def steamId(self):
         return self.steam_id
-   
-    def media(self):
-            return round(float(self.totalPontos / self.numeroRatings), 2)
 
+    def media(self):
+        return round(float(self.totalPontos / self.numeroRatings), 2)
 
 
 class ListaUtilizadorJogo(models.Model):
     class EstadosJogo(models.TextChoices):
-        COMPLETED = 'CM', ('Completed')
-        PLANTOPLAY = 'PP', ('Plan To Play')
-        DROPPED = 'DR', ('Dropped')
-        PLAYING = 'PL', ('Playing')
-        ONHOLD = 'OH', ('On Hold')
+        COMPLETED = 'CM', 'Completed'
+        PLANTOPLAY = 'PP', 'Plan To Play'
+        DROPPED = 'DR', 'Dropped'
+        PLAYING = 'PL', 'Playing'
+        ONHOLD = 'OH', 'On Hold'
 
     estado = models.CharField(
         max_length=2,
@@ -79,14 +75,14 @@ class ListaUtilizadorJogo(models.Model):
     )
     utilizador_id = models.ForeignKey(Utilizador, on_delete=models.CASCADE)
     jogo_id = models.ForeignKey(Jogo, on_delete=models.CASCADE)
-    rating = models.IntegerField(default=5)
+    rating = models.IntegerField(default=0)
 
 
 class Review(models.Model):
     listaUtliziadorJogo_id = models.OneToOneField(ListaUtilizadorJogo, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=127)
     texto = models.CharField(max_length=511)
-    
+
 
 class Gameplay(models.Model):
     titulo = models.CharField(max_length=127)
@@ -104,47 +100,31 @@ class ListaGameplays(models.Model):
     gameplay_id = models.ForeignKey(Gameplay, on_delete=models.CASCADE)
 
 
-class Thread(models.Model):
-    criador_id = models.ForeignKey(Utilizador, on_delete=models.SET_NULL, null=True)
-    jogo_id = models.ForeignKey(Jogo, on_delete=models.CASCADE)
-    titulo = models.CharField(max_length=127)
-    descricao = models.CharField(max_length=511)
-    data = models.DateTimeField('data de publicacao')
-    
-
-class Comentario(models.Model):
-    thread_id = models.ForeignKey(Thread, on_delete=models.CASCADE)
-    poster_id = models.ForeignKey(Utilizador, on_delete=models.CASCADE)
-    texto = models.CharField(max_length=255)
-    data = models.DateTimeField('data de publicacao')
-
-
-
 """
 
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=255)
-    
+
 class Personagem(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=255)
 
 
 class ListaJogoPersonagem(models.Model):
-    jogo_id = models.ForeignKey(Jogo, on_delete=models.CASCADE)
-    personagem_id = models.ForeignKey(Pesonagem, on_delete=models.CASCADE)
+    jogo_id = models.OneToManyField(Jogo, on_delete=models.CASCADE)
+    personagem_id = models.OneToManyField(Pesonagem, on_delete=models.CASCADE)
 
 class FotosJogo(models.Model):
-    jogo_id = models.ForeignKey(Jogo, on_delete=models.CASCADE)
+    jogo_id = models.OneToManyField(Jogo, on_delete=models.CASCADE)
     fotoLink = models.CharField(max_length=255)
 
     class TiposImagens(models.TextChoices):
         THUMBNAIL = 'TN', _('Thumbnail')
         BANNER = 'BN', _('Banner')
         INGAME = 'IG', _('In Game')
-        
+
 
     tipo = models.CharField(
         max_length=2,
